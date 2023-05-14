3558f7e9b4e3a634dbaa0585c6ae8eb46dc88363
42bchen <boyuchen0224@gmail.com>
2023-05-15 00:28:31 +0800

    Add UniswapV2Pracitce

---
+++ section3/UniswapV2Practice/contracts/test/TestERC20.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import { ERC20 } from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract TestERC20 is ERC20 {
+    uint8 internal _decimals;
+
+    constructor(string memory name, string memory symbol, uint8 decimalsArg) ERC20(name, symbol) {
+        _decimals = decimalsArg;
+    }
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
+    }
+}
---
+++ section3/UniswapV2Practice/test/UniswapV2Practice.t.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import { IUniswapV2Router01 } from "v2-periphery/interfaces/IUniswapV2Router01.sol";
+import { IUniswapV2Factory } from "v2-core/interfaces/IUniswapV2Factory.sol";
+import { IUniswapV2Pair } from "v2-core/interfaces/IUniswapV2Pair.sol";
+import { TestERC20 } from "../contracts/test/TestERC20.sol";
+
+contract UniswapV2PracticeTest is Test {
+    IUniswapV2Router01 public constant UNISWAP_V2_ROUTER =
+        IUniswapV2Router01(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
+    IUniswapV2Factory public constant UNISWAP_V2_FACTORY =
+        IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f);
+    address public constant WETH9 = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+
+    TestERC20 public testUSDC;
+    IUniswapV2Pair public WETHTestUSDCPair;
+    address public taker = makeAddr("Taker");
+    address public maker = makeAddr("Maker");
+
+    function setUp() public {
+        // fork block
+        vm.createSelectFork("mainnet", 17254242);
+
+        // deploy test USDC
+        testUSDC = _create_erc20("Test USDC", "USDC", 6);
+
+        // mint 100 ETH, 10000 USDC to maker
+        deal(maker, 100 ether);
+        testUSDC.mint(maker, 10000 * 10 ** testUSDC.decimals());
+
+        // mint 1 ETH to taker
+        deal(taker, 100 ether);
+    }
+
+    // #Practice1: create a pair for ETH, TestUSDC
+    function test_maker_create_pair() public {
+        // Implement here
+
+        // Checking
+        assertEq(WETHTestUSDCPair.token0(), address(testUSDC));
+        assertEq(WETHTestUSDCPair.token1(), address(WETH9));
+    }
+
+    // #Practice2: maker add liquidity (100 ETH, 10000 USDC)
+    function test_maker_addLiquidityETH() public {
+        // Implement here
+
+        // Checking
+        IUniswapV2Pair wethUsdcPair = IUniswapV2Pair(UNISWAP_V2_FACTORY.getPair(address(WETH9), address(testUSDC)));
+        (uint112 reserve0, uint112 reserve1, ) = wethUsdcPair.getReserves();
+        assertEq(reserve0, 10000 * 10 ** testUSDC.decimals());
+        assertEq(reserve1, 100 ether);
+    }
+
+    // #Practice3: taker swap exact 1 ETH for testUSDC
+    function test_taker_swapExactETHForTokens() public {
+        // Impelement here
+
+        // Checking
+        // #Practice4: discuss why 4992488733 ?
+        assertEq(testUSDC.balanceOf(taker), 4992488733);
+        assertEq(taker.balance, 0);
+    }
+
+    // #Practice5: taker swap exact 10000 USDC for ETH
+    function test_taker_swapExactTokensForETH() public {
+        // Impelement here
+
+        // Checking
+        // #Practice6: original balance is 100 ether, so delta is 49924887330996494742, but why 49924887330996494742 ?
+        assertEq(testUSDC.balanceOf(taker), 0);
+        assertEq(taker.balance, 149924887330996494742);
+    }
+
+    // #Practice7: maker remove all liquidity
+    function test_taker_removeLiquidityETH() public {
+        // Implement here
+
+        // Checking
+        IUniswapV2Pair wethUsdcPair = IUniswapV2Pair(UNISWAP_V2_FACTORY.getPair(address(WETH9), address(testUSDC)));
+        (uint112 reserve0, uint112 reserve1, ) = wethUsdcPair.getReserves();
+        assertEq(reserve0, 1);
+        assertEq(reserve1, 100000000);
+    }
+
+    function _create_erc20(string memory name, string memory symbol, uint8 decimals) internal returns (TestERC20) {
+        TestERC20 testERC20 = new TestERC20(name, symbol, decimals);
+        return testERC20;
+    }
+}
