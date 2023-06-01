04fa652b195cbf9bf405842f97306c3b8f997d5a
terri <mo0922@gmail.com>
2023-06-01 21:10:49 +0800

    CompoundPractice

--- section3/CompoundPractice/test/CompoundPractice.t.sol
+++ section3/CompoundPractice/test/CompoundPractice.t.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.19;
 
+import "forge-std/console.sol";
 import { EIP20Interface } from "compound-protocol/contracts/EIP20Interface.sol";
 import { CErc20 } from "compound-protocol/contracts/CErc20.sol";
 import "test/helper/CompoundPracticeSetUp.sol";
@@ -17,6 +18,9 @@ contract CompoundPracticeTest is CompoundPracticeSetUp {
   IBorrower public borrower;
 
   function setUp() public override {
+    uint256 forkId = vm.createFork("https://eth-mainnet.g.alchemy.com/v2/-CmqkVjFZ7gnwUdbfGpt_dasLBkBR1ds");
+    vm.selectFork(forkId);
+
     super.setUp();
 
     // Deployed in CompoundPracticeSetUp helper
@@ -34,22 +38,39 @@ contract CompoundPracticeTest is CompoundPracticeSetUp {
   function test_compound_mint_interest() public {
     vm.startPrank(user); 
     // TODO: 1. Mint some cUSDC with USDC
+    uint balance0 = USDC.balanceOf(user);
+    console.log(balance0);
+    USDC.approve(address(cUSDC), 10000 * 10 ** USDC.decimals());
+    assertEq(cUSDC.mint(10000 * 10 ** USDC.decimals()), 0);
 
     // TODO: 2. Modify block state to generate interest
+    vm.roll(block.number + 100);
 
     // TODO: 3. Redeem and check the redeemed amount
+    assertEq(cUSDC.redeem(cUSDC.balanceOf(user)), 0);
+    uint balance1 = USDC.balanceOf(user);
+    console.log(balance1);
+    assertGt(balance1, balance0);
   }
 
   function test_compound_mint_interest_with_borrower() public {
     vm.startPrank(user); 
     // TODO: 1. Mint some cUSDC with USDC
+    uint balance0 = USDC.balanceOf(user);
+    console.log(balance0);
+    USDC.approve(address(cUSDC), 10000 * 10 ** USDC.decimals());
+    assertEq(cUSDC.mint(10000 * 10 ** USDC.decimals()), 0);
 
     // 2. Borrower.borrow() will borrow some USDC
     borrower.borrow();
 
     // TODO: 3. Modify block state to generate interest
-
+    vm.roll(block.number + 100);
 
     // TODO: 4. Redeem and check the redeemed amount
+    assertEq(cUSDC.redeem(cUSDC.balanceOf(user)), 0);
+    uint balance1 = USDC.balanceOf(user);
+    console.log(balance1);
+    assertGt(balance1, balance0);
   }
 }
