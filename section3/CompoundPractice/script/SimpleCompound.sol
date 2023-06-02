437bd3fb20992846228898a0fff73d486e5f5ad8
Chienwang <songchien.wang@gmail.com>
2023-06-02 16:09:01 +0800

    feat: added week12-14 hw (Jon Wang)

---
+++ section3/CompoundPractice/script/SimpleCompound.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity ^0.8.17;
+
+import "forge-std/Script.sol";
+//cToken
+import "../contracts/CErc20Delegator.sol";
+import "../contracts/CErc20Delegate.sol";
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+//comptroller
+import "../contracts/Unitroller.sol";
+import "../contracts/Comptroller.sol";
+//interestModel
+import "../contracts/WhitePaperInterestRateModel.sol";
+//priceOracle
+import "../contracts/SimplePriceOracle.sol";
+
+contract MyScript is Script {
+    // oracle
+    SimplePriceOracle public priceOracle;
+    // whitepaper
+    WhitePaperInterestRateModel public whitePaper;
+    // comprtroller
+    Unitroller public unitroller;
+    Comptroller public comptroller;
+    Comptroller public unitrollerProxy;
+    // cToken
+    ERC20 public mt;
+    CErc20Delegate public cMTDelegate;
+    CErc20Delegator public cMT;
+
+    function run() external {
+        // get key
+        uint256 deployerPrivateKey = vm.envUint("key");
+        vm.startBroadcast(deployerPrivateKey);
+
+        // set oracle
+        priceOracle = new SimplePriceOracle(); // deploy oracle contract
+        // set whitepaper
+        whitePaper = new WhitePaperInterestRateModel(0, 0); // deploy interestRate contract
+        // set comptroller
+        unitroller = new Unitroller(); // deploy unitroller contract
+        comptroller = new Comptroller(); // deploy comptroller contract
+        unitrollerProxy = Comptroller(address(unitroller));
+        unitroller._setPendingImplementation(address(comptroller)); // set Implementation contract
+        comptroller._become(unitroller);
+        unitrollerProxy._setPriceOracle(priceOracle); // set oracle
+
+        // set cToken
+        mt = new ERC20("My Token", "MT"); // deploy erc20 contract, create MT token
+        cMTDelegate = new CErc20Delegate(); // deploy CErc20Delegate contract
+        bytes memory data = new bytes(0x00);
+        cMT = new CErc20Delegator(
+            address(mt),
+            ComptrollerInterface(address(unitroller)),
+            InterestRateModel(address(whitePaper)),
+            1e18,
+            "Compound My Token",
+            "cMT",
+            18,
+            payable(msg.sender),
+            address(cMTDelegate),
+            data
+        ); // deploy CErc20Delegator contract
+
+        vm.stopBroadcast();
+    }
+}
