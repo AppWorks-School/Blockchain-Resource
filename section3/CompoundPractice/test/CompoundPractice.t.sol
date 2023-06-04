8ddd4e705cc8e49ccd5dc054ea82e7745b3a59a7
Zephyr Wu <zephyrwu@cryptoblk.io>
2023-06-04 16:21:00 +0800

    fixed Compound test

--- section3/CompoundPractice/test/CompoundPractice.t.sol
+++ section3/CompoundPractice/test/CompoundPractice.t.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.19;
 import { EIP20Interface } from "compound-protocol/contracts/EIP20Interface.sol";
 import { CErc20 } from "compound-protocol/contracts/CErc20.sol";
 import "test/helper/CompoundPracticeSetUp.sol";
+import "forge-std/console.sol";
 
 interface IBorrower {
   function borrow() external;
@@ -17,6 +18,7 @@ contract CompoundPracticeTest is CompoundPracticeSetUp {
   IBorrower public borrower;
 
   function setUp() public override {
+    vm.createSelectFork("https://eth-mainnet.g.alchemy.com/v2/-J59kz0ImbkCPoQG2ftUEm4FPWL1dQza");
     super.setUp();
 
     // Deployed in CompoundPracticeSetUp helper
@@ -32,24 +34,43 @@ contract CompoundPracticeTest is CompoundPracticeSetUp {
   }
 
   function test_compound_mint_interest() public {
-    vm.startPrank(user); 
+    vm.startPrank(user);
+    uint256 amount = 100 * 10 ** USDC.decimals();
     // TODO: 1. Mint some cUSDC with USDC
+    USDC.approve(address(cUSDC), amount);
+    cUSDC.mint(amount);
+    uint256 beforeBalance = USDC.balanceOf(user);
 
     // TODO: 2. Modify block state to generate interest
+    vm.roll(block.number + 100);
 
     // TODO: 3. Redeem and check the redeemed amount
+    cUSDC.redeem(cUSDC.balanceOf(user));
+    uint256 afterBalance = USDC.balanceOf(user);
+    console.log(afterBalance);
+
+    vm.stopPrank();
+    assertGt(afterBalance, beforeBalance);
   }
 
   function test_compound_mint_interest_with_borrower() public {
-    vm.startPrank(user); 
+    vm.startPrank(user);
+    uint256 amount = 100 * 10 ** USDC.decimals();
+
     // TODO: 1. Mint some cUSDC with USDC
+    USDC.approve(address(cUSDC), amount);
+    cUSDC.mint(amount);
 
     // 2. Borrower.borrow() will borrow some USDC
     borrower.borrow();
 
     // TODO: 3. Modify block state to generate interest
-
+    vm.roll(block.number + 100);
 
     // TODO: 4. Redeem and check the redeemed amount
+    cUSDC.redeem(cUSDC.balanceOf(user));
+    uint256 postBalance = USDC.balanceOf(user);
+    console.log(postBalance);
+    vm.stopPrank();
   }
 }
