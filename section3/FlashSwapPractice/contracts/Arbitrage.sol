17bd7c2b9e8d43d00e7f4f5b6b4a0c438e4bed9c
shoyo <shoyo109527@gmail.com>
2023-11-25 23:56:55 +0800

    finish arbitrage contract

--- section3/FlashSwapPractice/contracts/Arbitrage.sol
+++ section3/FlashSwapPractice/contracts/Arbitrage.sol
@@ -9,6 +9,14 @@ import { IUniswapV2Callee } from "v2-core/interfaces/IUniswapV2Callee.sol";
 // This is a practice contract for flash swap arbitrage
 contract Arbitrage is IUniswapV2Callee, Ownable {
 
+    struct CallbackData {
+        address priceLowerPool;
+        address priceHigherPool;
+        uint256 borrowETH;
+        uint256 repayLowerPoolUSDC;
+        uint256 borrowHigherPoolUSDC;
+    } 
+
     //
     // EXTERNAL NON-VIEW ONLY OWNER
     //
@@ -27,7 +35,20 @@ contract Arbitrage is IUniswapV2Callee, Ownable {
     //
 
     function uniswapV2Call(address sender, uint256 amount0, uint256 amount1, bytes calldata data) external override {
-        // TODO
+        require(sender == address(this), "Sender is not this contract");
+        require(amount0 > 0 || amount1 > 0, "amount0 or amount1 should greater than zero");
+        
+        CallbackData memory callbackData = abi.decode(data, (CallbackData));
+        require(msg.sender == callbackData.priceLowerPool, "msg.sender is not priceLowerPool");
+
+        // token0 是 WETH
+        address token0 = IUniswapV2Pair(callbackData.priceLowerPool).token0();
+        // 用 WETH 去 priceHigherPool 換出 USDC
+        IERC20(token0).transfer(callbackData.priceHigherPool, callbackData.borrowETH);
+        IUniswapV2Pair(callbackData.priceHigherPool).swap(0, callbackData.borrowHigherPoolUSDC, address(this), "");
+        // 還 USDC 給 priceLowerPool
+        address token1 = IUniswapV2Pair(callbackData.priceLowerPool).token1();
+        IERC20(token1).transfer(callbackData.priceLowerPool, callbackData.repayLowerPoolUSDC);
     }
 
     // Method 1 is
@@ -40,7 +61,22 @@ contract Arbitrage is IUniswapV2Callee, Ownable {
     //  - repay WETH to higher pool
     // for testing convenient, we implement the method 1 here
     function arbitrage(address priceLowerPool, address priceHigherPool, uint256 borrowETH) external {
-        // TODO
+        // 1. 計算在需提供多少 USDC 去 lower price pool 借出 borrowETH 
+        (uint112 reserve0, uint112 reserve1,) = IUniswapV2Pair(priceLowerPool).getReserves();
+        uint256 amountIn = _getAmountIn(borrowETH, uint256(reserve1), uint256(reserve0));
+
+        // 2. 計算用 borrowETH 在 higher price pool 可以換多少 USDC
+        (reserve0, reserve1,) = IUniswapV2Pair(priceHigherPool).getReserves();
+        uint256 amountOut = _getAmountOut(borrowETH, uint256(reserve0), uint256(reserve1));
+        
+        CallbackData memory callbackData = CallbackData({
+            priceLowerPool: priceLowerPool,
+            priceHigherPool: priceHigherPool,
+            borrowETH: borrowETH,
+            repayLowerPoolUSDC: amountIn,
+            borrowHigherPoolUSDC: amountOut
+        });
+        IUniswapV2Pair(priceLowerPool).swap(borrowETH, 0, address(this), abi.encode(callbackData));
     }
 
     //
