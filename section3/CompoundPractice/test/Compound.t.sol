b8c1c7cf548d5a4ab99effa0d495d6025f64c5d1
chiwei.huang <chiwei.huang@chiweihuangdeMacBook-Air.local>
2023-12-09 07:31:04 +0800

    setup

--- section3/CompoundPractice/test/Compound.t.sol
+++ section3/CompoundPractice/test/Compound.t.sol
@@ -13,63 +13,60 @@ import "compound-protocol/contracts/PriceOracle.sol";
 import "contracts/MyToken.sol";
 
 contract Compound is Test {
-    function deployDelegator(ERC20 token) public {
-        underlying_ = address(token);
-        // 2. Deploy Comptroller
-            // 2.1 Deploy Comptroller Implementation contract (Comptroller)
-            Comptroller comptroller = new Comptroller();
-            // 2.2 Deploy Comptroller Proxy contract (Unitroller)
-            Unitroller unitroller = new Unitroller();
-            // 2.3 Set Implentation contract address
-            uint errorCode = unitroller._setPendingImplementation(address(comptroller));
-            require(errorCode == 0, "Set Implementation Failed");
-            // 2.4 Change brain (Proxy address) for Comptroller using _become function
-            comptroller._become(unitroller);
-            // 2.5 Set Price Oracle
-            SimplePriceOracle simplePriceOracle = new SimplePriceOracle();
-            comptroller_ = Comptroller(address(unitroller));
-            comptroller_._setPriceOracle(PriceOracle(simplePriceOracle));
-        // 3. Deploy InterestRateModel -> WhitePaperInterestRateModel, rate = 0%
-            WhitePaperInterestRateModel interestRateModel = new WhitePaperInterestRateModel(0, 0);
-            interestRateModel_ = InterestRateModel(address(interestRateModel));
-        // 4. initialExchangeRateMantissa_ -> 1e18
-            initialExchangeRateMantissa_ = 1 ether;
-        // 5. name_ -> ctoken name
-            name_ = token.name ;
-        // 6. symbol_ -> ctoken symbol
-            symbol_ = token.symbol;
-        // 7. decimals_ -> 18
-            decimals_ = 18;
-        // 8. admin_ -> address(this)
-            admin_ = payable(address(this));
-        // 9. implementation_ -> CErc20Delegate
-            CErc20Delegate cErc20Delegate = new CErc20Delegate();
-            implementation_ = address(cErc20Delegate);
-        // 10. becomeImplementationData -> unused, 0x00
-            becomeImplementationData = new bytes(0x00);
-
-        cToken = new CErc20Delegator(
-            underlying_,
-            comptroller_,
-            interestRateModel_,
-            initialExchangeRateMantissa_,
-            name_,
-            symbol_,
-            decimals_,
-            admin_,
-            implementation_,
-            becomeImplementationData
-        );
-    }
+    Comptroller public comptroller;
+    InterestRateModel public interestRateModel;
+    WhitePaperInterestRateModel public interestRateModel;
+    SimplePriceOracle public simplePriceOracle;
 
 
     function setup() public {
+        // deploy comptroller
+        Comptroller comptroller = new Comptroller();
+        Unitroller unitroller = new Unitroller();
+        uint errorCode = unitroller._setPendingImplementation(address(comptroller));
+        require(errorCode == 0, "Set Implementation Failed");
+        comptroller._become(unitroller);
+        // oracle
+        simplePriceOracle = new SimplePriceOracle();
+        comptroller = Comptroller(address(unitroller));
+        comptroller._setPriceOracle(PriceOracle(simplePriceOracle));
+        interestRateModel = new WhitePaperInterestRateModel(0, 0);
+
         // Deploy cERC20 tokenA
         tokenA = new MyToken("tokenA", "tokenA");
-        deployDelegator(tokenA);
+        CErc20Delegate cErc20Delegate = new CErc20Delegate();
+        cToken = new CErc20Delegator(
+            address(token),
+            comptroller,
+            InterestRateModel(address(interestRateModel)),
+            1 ether,
+            token.name,
+            token.symbol,
+            18,
+            payable(address(this)),
+            address(cErc20Delegate),
+            becomeImplementationData
+        );
+        comptroller._supportMarket(cToken(address(tokenA)));
+
         // Deploy cERC20 tokenB
-        tokenB = new MyToken("tokenB", "tokenB");
-        deployDelegator(tokenB);
+        tokenA = new MyToken("tokenA", "tokenA");
+        CErc20Delegate cErc20Delegate = new CErc20Delegate();
+        cToken = new CErc20Delegator(
+            address(token),
+            comptroller,
+            InterestRateModel(address(interestRateModel)),
+            1 ether,
+            token.name,
+            token.symbol,
+            18,
+            payable(address(this)),
+            address(cErc20Delegate),
+            becomeImplementationData
+        );
+        comptroller._supportMarket(cToken(address(tokenB)));
+
+        // users
         user1 = makeAddr("user1");
         user2 = makeAddr("user2");
     }
