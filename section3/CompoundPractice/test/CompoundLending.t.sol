2d3c4bda87989987171de63e1093b3470106dec4
chiwei.huang <chiwei.huang@chiweihuangdeMacBook-Air.local>
2023-12-09 19:40:56 +0800

    testLiquidateByAdjustTokenBPrice

--- section3/CompoundPractice/test/CompoundLending.t.sol
+++ section3/CompoundPractice/test/CompoundLending.t.sol
@@ -242,6 +242,69 @@ contract CompoundLending is Test {
     }
 
     function testLiquidateByAdjustTokenBPrice() public {
+        // user1 mint cTokenB and use cToken as collateral for borrow TokenA
+        uint256 initialTokenAAmount = 100 * 10 ** tokenA.decimals();
+        uint256 initialTokenBAmount = 100 * 10 ** tokenB.decimals();
+        uint256 mintTokenBAmount = 1 * 10 ** tokenB.decimals();
+        uint256 borrowTokenAAmmount = 50 * 10 ** tokenA.decimals();
 
+        // Give user1 token A, token B initial amount
+        deal(address(tokenA), user1, initialTokenAAmount);
+        deal(address(tokenB), user1, initialTokenBAmount);
+
+        // Give cTokenA enough tokenA to borrow
+        deal(address(tokenA), address(cTokenA), 30000 * 10 ** tokenA.decimals());
+
+        vm.startPrank(user1);
+
+        // user 1 use 1 token B to mint cTokenB
+        ERC20(tokenB).approve(address(cTokenB), mintTokenBAmount);
+        cTokenB.mint(mintTokenBAmount);
+
+        assertEq(tokenB.balanceOf(user1), initialTokenBAmount - mintTokenBAmount);
+        assertEq(cTokenB.balanceOf(user1), mintTokenBAmount);
+
+        // user 1 use tokenB as collateral for borrow 50 tokenA
+        // enter cTokenB market
+        address[] memory addr = new address[](1);
+        addr[0] = address(cTokenB);
+        comptrollerProxy.enterMarkets(addr);
+        
+        // borrow 50 tokenA
+        cTokenA.borrow(borrowTokenAAmmount);
+
+        assertEq(tokenA.balanceOf(user1), initialTokenAAmount + borrowTokenAAmmount);
+
+        vm.stopPrank();
+
+        // Adjust Token B price
+        simplePriceOracle.setUnderlyingPrice(CToken(address(cTokenB)), 90);
+
+        vm.startPrank(user2);
+
+        // check shortfall
+        // The borrower must have shortfall in order to be liquidatable
+        (, , uint shortfall) = comptrollerProxy.getAccountLiquidity(user1);
+        require(shortfall > 0, "no shortfall, not liquidatable");
+
+        // give user2 enough tokenA to liquidateBorrow
+        deal(address(tokenA), user2, initialTokenAAmount);
+
+        // approve cTokenA to use user2's tokenA to liquidate
+        tokenA.approve(address(cTokenA),  25 * 10 ** tokenA.decimals());
+
+        uint256 errCode = cTokenA.liquidateBorrow(user1, 25 * 10 ** tokenA.decimals(), cTokenB);
+
+        require(errCode == 0, "liquidateBorrow failed");
+
+        // As a liquidator, user2 can get user1's collateral cTokenB
+
+        // user 2 use tokenA to repay
+        assertEq(tokenA.balanceOf(user2), initialTokenAAmount - 25 * 10 ** tokenA.decimals(), "user2 balance A is incorrect");
+
+        // user 2 get 5 %( liquidation incentive) of collateral cTokenB as reward
+        assertGt(cTokenB.balanceOf(user2), 0, "user 2 cTokenB balance should greate than 0");
+
+        vm.stopPrank();
     }
 }
