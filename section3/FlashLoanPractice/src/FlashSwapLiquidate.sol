aea60137dbaf3a7e1d1697504c039da6c2659ac5
chiwei.huang <chiwei.huang@chiweihuangdeMacBook-Air.local>
2023-12-14 20:36:22 +0800

    FlashSwapLiquidate test

--- section3/FlashLoanPractice/src/FlashSwapLiquidate.sol
+++ section3/FlashLoanPractice/src/FlashSwapLiquidate.sol
@@ -16,12 +16,53 @@ contract FlashSwapLiquidate is IUniswapV2Callee {
   IUniswapV2Router02 public router = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
   IUniswapV2Factory public factory = IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f);
 
+  struct CallbackData {
+    address borrower;
+    address tokenIn;
+    address tokenOut;
+    uint256 amountIn; // repay amount
+    uint256 amountOut; // borrow amount
+  }
+
   
   function uniswapV2Call(address sender, uint256 amount0, uint256 amount1, bytes calldata data) external override {
     // TODO
+    require(sender == address(this), "Sender must be this contract");
+    require(amount0 > 0 || amount1 > 0, "Amount must be greater than 0");
+
+    // // 4. decode callback data
+    CallbackData memory callbackData = abi.decode(data, (CallbackData));
+
+    // // 5. call liquidation
+    IERC20(callbackData.tokenOut).approve(address(cUSDC), callbackData.amountOut); // approve USDC to lending protocol cUSDC
+    cUSDC.liquidateBorrow(callbackData.borrower, callbackData.amountOut, cDAI); // get cDai
+
+    // 6. redeem cDai
+    cDAI.redeem(cDAI.balanceOf(address(this)));
+
+    // 7. repay cDai to uniswap pool
+    DAI.transfer(msg.sender, callbackData.amountIn);
+
+    // // check profit
+    // require(address(this).balance >= _MINIMUM_PROFIT, "Profit must be greater than 0.01 ether");
   }
 
   function liquidate(address borrower, uint256 amountOut) external {
     // TODO
+
+    // Call swap on pair to get USDC
+    address[] memory path = new address[](2);
+    path[0] = address(DAI);
+    path[1] = address(USDC);
+
+    // // 1. get uniswap pool address
+    // address pair = IUniswapV2Factory(_UNISWAP_FACTORY).getPair(path[0], path[1]);
+    address pair = factory.getPair(address(DAI), address(USDC));
+
+    // // 2. calculate repay amount
+    uint256[] memory amountsIn = router.getAmountsIn(amountOut, path);
+    CallbackData memory callbackData = CallbackData(borrower, path[0], path[1], amountsIn[0], amountOut);
+    // // 3. flash swap from the uniswap pool
+    IUniswapV2Pair(pair).swap(0, amountOut, address(this), abi.encode(callbackData));
   }
 }
